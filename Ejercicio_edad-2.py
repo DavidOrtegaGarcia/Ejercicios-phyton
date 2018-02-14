439ba10c20b9268791a1cdbd2dac58e06ce3633e
DavidOrtegaGarcia <36156063+DavidOrtegaGarcia@users.noreply.github.com>
2018-02-14 18:52:07 +0100

    Create Ejercicio_edad-2.py

---
+++ Ejercicio_edad-2.py
@@ -0,0 +1,10 @@
+# coding: utf8
+# David Ortega Garcia
+# 14/02/2018
+
+edad=input("Escriba su edad:")
+
+if (edad>17 and edad<24):
+    print "Puede entrar en la sesión de jovenes"
+else:
+    print "No puede entrar en la sesión de jovenes"
