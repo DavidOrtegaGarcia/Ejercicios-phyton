52bbaed9fde1cfe7acbf7a4d755a7291831999e2
DavidOrtegaGarcia <36156063+DavidOrtegaGarcia@users.noreply.github.com>
2018-02-14 20:25:55 +0100

    Update ejercicio-pares.py

--- ejercicio-pares.py
+++ ejercicio-pares.py
@@ -2,8 +2,8 @@
 # David Ortega Garcia
 # 08/02/2018
 
-numero=input "Posar un numero"
-if numero%==0:
+numero=input ("Posar un numero")
+if (numero%==0):
     print "Que bonito numero par"
 else
     print "Que numero mas bulgar"
