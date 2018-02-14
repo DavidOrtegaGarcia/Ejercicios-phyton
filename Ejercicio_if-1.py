9b5395a99fc524f209262a59724036828a15b664
DavidOrtegaGarcia <36156063+DavidOrtegaGarcia@users.noreply.github.com>
2018-02-14 19:48:38 +0100

    Create Ejercicio_if-1.py

---
+++ Ejercicio_if-1.py
@@ -0,0 +1,13 @@
+# coding: utf8
+# David Ortega Garcia
+# 14/02/2018
+
+dividendo=input ("Escriba el dividendo:")
+divisor=input ("Escriba el dividsor:")
+cociente= dividendo/divisor
+residuo=dividendo%divisor
+
+if (reso==0):
+    print "La division es exacta. Cociente:",cociente
+else:
+    print "La division no es exacta. Cociente","Residuo:",residuo
