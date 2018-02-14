083efda236e37c9a3129518cb62f116728eb5460
DavidOrtegaGarcia <36156063+DavidOrtegaGarcia@users.noreply.github.com>
2018-02-14 23:27:37 +0100

    Update ejercicio-condicion-chunga.py

--- ejercicio-condicion-chunga.py
+++ ejercicio-condicion-chunga.py
@@ -3,7 +3,8 @@
 # 08/02/2018
 
 numero=input "Posar un numero"
-if numero%==0
-   or (numero>-10 and numero<=40)
-   or (numero<=-1):
-	  print "Cumple la condicion"
+
+if (numero%==0) or (numero>-10 and numero<=40) or (numero<=-1):
+    print "Cumple la condicion"
+else:
+    print "No cumple la condicion"
