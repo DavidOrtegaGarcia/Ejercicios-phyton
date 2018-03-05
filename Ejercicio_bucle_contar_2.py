c175a81f94b090dc4f06e5739c7df12140b6875b
DavidOrtegaGarcia <36156063+DavidOrtegaGarcia@users.noreply.github.com>
2018-03-05 21:18:05 +0100

    Create Ejercicio_bucle_contar_2.py

---
+++ Ejercicio_bucle_contar_2.py
@@ -0,0 +1,20 @@
+# coding:utf8
+# David Ortega Garcia
+# 02/03/2018
+
+# Inicializaciones
+salir = "N"
+liminte= input ("Hasta que limite amo?:")
+num=1
+
+if (num<=0):
+    salir="S"
+while ( salir=="N" ):
+    # Hago cosas
+    print num
+
+    # Incremento
+    num=num+1
+    # Activo indicador de salida si toca
+    if ( numero>limite ): # Condición de salida
+            salir = "S"
